a171ce39270ba8cbb109fd41a3b400263045d84d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-07-25 10:07:09 +0200

    Setup and initialize PeerJS

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Script from 'next/script';
 
 const Room = () => {
   return (
-    <h1 className='mt-20 text-center text-3xl uppercase font-black'>Room</h1>
+    <>
+      <Script
+        src='https://unpkg.com/peerjs@1.4.5/dist/peerjs.min.js'
+        onLoad={() => {
+          console.log(Peer);
+        }}
+      />
+
+      <h1
+        className='mt-20 text-center text-3xl uppercase 
+					font-black'
+      >
+        Room
+      </h1>
+    </>
   );
 };
 
